c679a6ba42d50181d6babfb4e84b9beb95f75758
Irtiza Ali <aliirtiza643@gmail.com>
2025-04-23 23:26:47 +0100

    creating notes

--- 01_intro.py
+++ 01_intro.py
@@ -1,1 +1,11 @@
 print('python is a vertasyle language , which is use in multiple domains like Data Science , Data Engineering , Automation , AI , ML and Web Developing')
+
+
+# Data Types :
+# 
+# '''' 
+# There are two major datatypes in python 
+#  mutable and immmutable 
+#  mutable datatypes :values can be change or modify after assigning 
+#  immutable datatypes : values cant be change after assigning 
+# '''
